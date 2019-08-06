82a9a73304c8188ae66b170bda3209345fcd7a8a
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-08-06 17:09:24 +0300

    Update TestCycle.sh

--- TestCycle.sh
+++ TestCycle.sh
@@ -23,7 +23,7 @@ do
    sed 's#Builder().forBrowser('"'"'firefox'"'"')#Builder().usingServer('"'"'http://localhost:4444/wd/hub'"'"').forBrowser('"'"'chrome'"'"')#g' "$i" > "$i"
    SFLOGIN="await driver.get('$VERSION_ID')"
    sed 's#var={}#$SFLOGIN#g' "$i" > index-updated.txt
-   cat index-updated.txt
+   head -n15 index-updated.txt
   # mocha index-updated.js || key=false 
 done
 if [ "$key" = "false" ]
