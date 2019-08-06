09a639188e173cd48aa5cc1717d3ec7f3c9d3254
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-08-06 12:42:21 +0300

    Update 0Amount.js

--- 0Amount.js
+++ 0Amount.js
@@ -2,7 +2,7 @@
 const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
 
-describe('0 Amount', function() {
+describe('0 Amount Cash (false)', function() {
   this.timeout(30000)
   let driver
   let vars
@@ -13,29 +13,26 @@ describe('0 Amount', function() {
   afterEach(async function() {
     await driver.quit();
   })
-  it('0 Amount', async function() {
-    await driver.get("https://beyondblue--dev.lightning.force.com/lightning/r/Contact/0030l00000UedkOAAR/view?0.source=alohaHeader")
-    await driver.findElement(By.css(".slds-button:nth-child(1) > .forceActionLink > .slds-truncate")).click()
+  it('0 Amount Cash (false)', async function() {
+    await driver.get("https://beyondblue--dev.lightning.force.com/lightning/n/Bulk_Donations?0.source=alohaHeader")
+    await driver.findElement(By.xpath("//td[3]/div/div/div[2]/div/input")).click()
+    await driver.findElement(By.xpath("//td[3]/div/div/div[2]/div/input")).sendKeys("mr selen")
+    await driver.findElement(By.css(".slds-lookup__list:nth-child(2) .slds-media")).click()
+    {
+      const element = await driver.findElement(By.css("td:nth-child(3) .slds-button .slds-icon"))
+      await driver.actions({ bridge: true }).moveToElement(element).perform()
+    }
+    await driver.findElement(By.xpath("//td[4]/div/div/div[2]/div/input")).click()
+    await driver.findElement(By.xpath("//td[4]/div/div/div[4]/ul/li/span/div/span")).click()
+    await driver.findElement(By.xpath("//lightning-input/div/input")).click()
     await driver.findElement(By.xpath("//lightning-input/div/input")).sendKeys("0")
-    await driver.findElement(By.xpath("//div[2]/div/div/div/div[2]/div/input")).click()
-    await driver.findElement(By.css(".slds-lookup__list:nth-child(2) > li:nth-child(1) > .slds-lookup__item-action")).click()
-    await driver.findElement(By.xpath("//div[4]/div[2]/div/lightning-input/div/input")).click()
-    await driver.findElement(By.xpath("//div[4]/div[2]/div/lightning-input/div/input")).sendKeys("4444333322221111")
-    await driver.findElement(By.xpath("//div[3]/div/lightning-input/div/input")).click()
-    await driver.findElement(By.xpath("//div[3]/div/lightning-input/div/input")).sendKeys("ddd")
-    await driver.findElement(By.xpath("//select")).click()
+    await driver.findElement(By.xpath("//td[7]/div/div/div/select")).click()
     {
-      const dropdown = await driver.findElement(By.xpath("//select"))
-      await dropdown.findElement(By.css("*:nth-child(2)")).click()
+      const dropdown = await driver.findElement(By.xpath("//td[7]/div/div/div/select"))
+      await dropdown.findElement(By.css("*:nth-child(1)")).click()
     }
-    await driver.findElement(By.xpath("//div[2]/div/div/div/select")).click()
-    {
-      const dropdown = await driver.findElement(By.xpath("//div[2]/div/div/div/select"))
-      await dropdown.findElement(By.css("*:nth-child(5)")).click()
-    }
-    await driver.findElement(By.xpath("//div[3]/lightning-input/div/input")).click()
-    await driver.findElement(By.xpath("//div[3]/lightning-input/div/input")).sendKeys("123")
     await driver.findElement(By.css(".slds-button_brand")).click()
-    assert(await driver.findElement(By.css(".pageLevelErrors .genericNotification")).getText() == "Errors:")
+    await driver.findElement(By.xpath("//button[contains(.,\'Save\')]")).click()
+    await driver.wait(until.elementLocated(By.css(".slds-theme--error")), 12000)
   })
 })
