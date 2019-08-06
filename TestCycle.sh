11f9def0573a8ec358b4cece501bc43564e0a479
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-08-06 14:57:50 +0300

    Update TestCycle.sh

--- TestCycle.sh
+++ TestCycle.sh
@@ -1,6 +1,20 @@
+if [ "${SFDX_AUTH_URL}" != "" ]
+then
+sudo npm install -global sfdx-cli
 sudo npm install -g json
-VERSION_ID=`sfdx force:org:open -u "ORG_NAME" --json | json result.url`
-echo "$VERSION_ID"
+sudo npm install -g slack-cli
+FILE_NAME="sfdx-auth-url.txt"
+# Creation of a temp file that contains Sfdx Auth Url
+echo ${SFDX_AUTH_URL} > ${FILE_NAME}
+# Login
+echo "Authenticating..."
+sfdx force:auth:sfdxurl:store -f ${FILE_NAME} -d -a DevHub
+# Removing the file
+rm ${FILE_NAME}
+else
+    echo "No SFDX_AUTH_URL variable. The test are run only for public available pages"
+fi
+
 key=true
 for i in *.js;
 do
