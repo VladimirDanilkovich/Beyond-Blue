49ae0374300d7d00211c8249d476d506906f2d5c
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-08-06 15:52:20 +0300

    Update TestCycle.sh

--- TestCycle.sh
+++ TestCycle.sh
@@ -23,7 +23,7 @@ do
    SFLOGIN="var={}   await driver.get('$VERSION_ID')"
    echo "$SFLOGIN"
    sed 's#var={}#$SFLOGIN#g' "$i" > index-updated.js
-   
+   egrep -v index-updated.js | uniq
    sed 's#Builder().forBrowser('"'"'firefox'"'"')#Builder().usingServer('"'"'http://localhost:4444/wd/hub'"'"').forBrowser('"'"'chrome'"'"')#g' index-updated.js > index-updated.js
    mocha index-updated.js || key=false 
 done
