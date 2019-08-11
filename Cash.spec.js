b0b658e320f48cfa5c1c603c754e138fe8cfcbdc
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-08-11 17:11:29 +0300

    Update and rename NEW_cash.spec.js to Cash.spec.js

--- Cash.spec.js
+++ Cash.spec.js
@@ -2,7 +2,7 @@
 const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
 
-describe('New Cash', function() {
+describe('Cash', function() {
   this.timeout(30000)
   let driver
   let vars
